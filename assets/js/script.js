b8e5411df827c332c4999be00ae0acb15abbe680
Raghad Malallah <raghadmalallah@gmail.com>
2021-10-03 21:35:39 -0700

    setting the var for the first 3 keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-///Your API key is e155d77d5c8ef4f402dee9ec6ded3ca5
+var APIKey = "e155d77d5c8ef4f402dee9ec6ded3ca5";
+
+var inputEl = document.getElementById("city-input");
+    var searchEl = document.getElementById("search-button");
+    var clearEl = document.getElementById("clear-history");
+    var nameEl = document.getElementById("city-name");
+    
+
+    
